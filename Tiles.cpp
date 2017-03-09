d71b6c87c93dd6f4911b00da77a55ec34c67a4c0
Apoorva Dheeraj <maildheerajniec26@gmail.com>
2017-03-10 00:40:27 +0530

    Tiles File added

---
+++ Tiles.cpp
@@ -0,0 +1,9 @@
+#include "Tiles.h"
+
+Tiles::Tiles(int type_ofTile , int row , int column){
+
+	this->type_ofTile = type_ofTile;
+	this->row = row;
+	this->column = column;
+	this->tile = nullptr;
+}
